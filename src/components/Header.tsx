6e651f28bb3c7e69aa9d6be531a3b6b5131a9653
ShayMashiah <Shay2908@gmail.com>
2025-01-18 22:08:03 +0200

    Creation of the header and title

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import "../css/Header.css"
+
+const Header = () => {
+  return (
+    <nav className="header">
+        <div className="header-center">
+            <h3> TicTacToe</h3>
+        </div>
+    </nav>
+  );
+}
+export default Header;
+
